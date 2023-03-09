9cf896f1cf921cb1687566268395ebaabb8961af
Mashrur Rahman <127143435+RawnokRahman@users.noreply.github.com>
2023-03-10 00:03:54 +0600

    Add files via upload

---
+++ genymotion.sh
@@ -0,0 +1,7 @@
+cd ~/
+wget https://dl.genymotion.com/releases/genymotion-3.3.3/genymotion-3.3.3-linux_x64.bin
+sudo bash genymotion-3.3.3-linux_x64.bin
+cd /opt/genymobile/genymotion/
+sudo ./genymotion
+sudo apt install lolcat
+echo 'installation finished now you can lunch genymotion in your application luncher or run this command " cd /opt/genymobile/genymotion/ && sudo ./genymotion " and then please create a account or use your old account and then Enjoy' | lolcat
